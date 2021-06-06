721ed5f372a11cee49c5441544908fb48703fe84
Jiří <xhanj02@gmail.com>
2021-06-06 16:38:49 +0200

    Fix "Add ingredients amount"

--- abl/recipe/update-abl.js
+++ abl/recipe/update-abl.js
@@ -3,14 +3,14 @@ const CookBookDao = require("../../dao/recipe-dao");
 let dao = new CookBookDao(path.join(__dirname, "..", "..", "storage", "recipes.json"))
 
 async function UpdateAbl(req, res) {
-    let {id, name, description, ingredientList, ingredientMap ? ingredientMap :} = req.body;
+    let {id, name, description, ingredientList} = req.body;
     if (
         (id && typeof id === "string" && id.length < 25) &&
         (name && typeof name === "string" && name.length < 200) &&
         (ingredientList && ingredientList.length > 0 && ingredientList.length < 10) &&
         (description && typeof description === "string" && description.length < 200)
     ) {
-        const recipe = {id, name, description, ingredientList, ingredientMap ? ingredientMap :};
+        const recipe = {id, name, description, ingredientList};
         try {
             let result = await dao.updateRecipe(recipe);
             res.status(200).json(result);
